0ae9d3ab2a0ed66a4d72cac291c390a3e3d09b0a
EdgarJe <89488314+EdgarJe@users.noreply.github.com>
2022-07-01 18:12:03 -0500

    Update ResultsScreen.js

---
+++ Screens/ResultsScreen.js
@@ -0,0 +1,16 @@
+import React,{Component} from "React";
+import { View, TextInput, Text, TouchableOpacity, StyleSheet,
+Image } from "react-native-web";
+
+export default class ResultsScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Resultados de la búsqueda
+                </Text>
+            </View>
+        );
+
+    }
+}
